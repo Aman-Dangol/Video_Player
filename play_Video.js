53c6bccd2adf52722e0cb439506beae973bcadc2
Aman Dangol <amandangol423@gmail.com>
2023-06-11 17:57:53 +0545

    made some changes to webPage

--- play_Video.js
+++ play_Video.js
@@ -2,12 +2,17 @@
     var file = document.getElementById('file');
     var videoPlayer = document.getElementById('videoPlayer');
     var url="";
-    file.addEventListener('change', function(event) {
-      var selectedFile = event.target.files[0];
-       url = URL.createObjectURL(selectedFile);
+    file.addEventListener('change',  function(event){
+        getUrl(event);
     });
 
     function play(){
         videoPlayer.src=url;
 
+    }
+    function getUrl(event){
+        {
+      var selectedFile = event.target.files[0];
+       url = URL.createObjectURL(selectedFile);
+    }
     }
